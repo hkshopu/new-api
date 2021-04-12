f80492f9d6d194d49474a592bfbc9a6bbc4c093a
Chris Chen <chrischen@times-transform.com>
2021-04-12 16:45:01 +0800

    hkshopu-new-api

--- store/urls.py
+++ store/urls.py
@@ -31,5 +31,6 @@ urlpatterns = [
     path('selected_product_color/', include('selected_product_color.urls')), 
     path('selected_product_size/', include('selected_product_size.urls')), 
     path('selected_product_pic/', include('selected_product_pic.urls')), 
-    path('selected_shop_category/', include('selected_shop_category.urls'))
+    path('selected_shop_category/', include('selected_shop_category.urls')),
+    path('shop_sub_category/', include('shop_sub_category.urls'))
 ]
