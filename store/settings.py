5479dc33a247f410074b6e5501f01e7ff5716db3
Eric You <yoericyou@times-transform.com>
2021-04-22 11:59:35 +0800

    hkshopu-new-api

--- store/settings.py
+++ store/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'NAME': 'store',
         'USER': 'root',
         'PASSWORD': '1@3$qWeR',
-        'HOST': '/cloudsql/hkshopu-20700:asia-east2:hkshopu',
+        'HOST': '/cloudsql/hkshopu:asia-east2:hkshopu',
         'PORT': '3306',
     }
 }
