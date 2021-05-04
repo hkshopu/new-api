75b0d4ac68fb9d1e69fda2f65e58c043443f1c5d
Eric You <yoericyou@times-transform.com>
2021-05-04 16:07:26 +0800

    get product average rating of specific shop

--- shop/urls.py
+++ shop/urls.py
@@ -13,5 +13,6 @@ urlpatterns = [
     path('bankAccount/<id>/delete/', views.delBankAccount),
     path('test/', views.testAPI), 
     path('<id>/get_product_quantity_of_specific_shop/', views.get_product_quantity_of_specific_shop), 
-    path('<id>/get_follower_quantity_of_specific_shop/', views.get_follower_quantity_of_specific_shop)
+    path('<id>/get_follower_quantity_of_specific_shop/', views.get_follower_quantity_of_specific_shop), 
+    path('<id>/get_product_average_rating_of_specific_shop/', views.get_product_average_rating_of_specific_shop)
 ]
