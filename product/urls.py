a9a037d6e24ea08d6b8a8c63a2af1508375a7373
chrischen-times-transform <chrischen@times-transform.com>
2021-05-19 18:23:32 +0800

    update_product_status_forAndroid

--- product/urls.py
+++ product/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     path('<id>/update/', views.update),  
     path('<id>/<keyword>/<product_status>/<quantity>/product_list/', views.product_list),
     path('update_product_status/', views.update_product_status),
+    path('update_product_status_forAndroid/', views.update_product_status_forAndroid),
     path('<id>/delete_product/', views.delete_product),
 ]
