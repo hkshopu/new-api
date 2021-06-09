9b7eda10cdcfc6e3f20e8a8b38feaead4837d19f
Roger Wang <rogerwang@times-transform.com>
2021-06-09 11:21:02 +0800

    models audit log

--- hkshopu/models.py
+++ hkshopu/models.py
@@ -682,7 +682,7 @@ class Email_Validation(models.Model):
 
 class Audit_Log(models.Model):
     id = models.CharField(primary_key=True, max_length=36)
-    user_id = models.IntegerField()
+    user_id = models.CharField(max_length=11)
     action = models.CharField(max_length=100)
     parameter_in = models.TextField()
     parameter_out = models.TextField()
