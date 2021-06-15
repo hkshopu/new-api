834e4a7830dc00406da915eb55dcb5fc86f60b6f
ericyou-times-transform <76938542+ericyou-times-transform@users.noreply.github.com>
2021-06-15 18:03:06 +0800

    add "get_all_audit_logs_page" URL

--- serback/urls.py
+++ serback/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('audit_logs/', views.get_all_audit_logs)
+    path('get_all_audit_logs_page/', views.get_all_audit_logs_page),
+    path('get_all_audit_logs/', views.get_all_audit_logs)
 ]
