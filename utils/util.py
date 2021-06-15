9e88506ad4d84d3431ca8cab4daf74747afdc521
Roger Wang <rogerwang@times-transform.com>
2021-06-15 10:58:45 +0800

    error except util

--- utils/util.py
+++ utils/util.py
@@ -2,3 +2,7 @@
 
 def strtobool(s):
     return s.lower() in ['true', '1', 't', 'y', 'yes', 'on']
+
+class ErrorWithArgs(Exception):
+    def __init__(self, *args):
+        self.args = [a for a in args]
