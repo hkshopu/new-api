dd93a8b7a4eb70e24cce70f14c6dafcf4a2eebe3
ericyou-times-transform <76938542+ericyou-times-transform@users.noreply.github.com>
2021-06-16 17:54:02 +0800

    update get_all_audit_logs_page

--- templates/static/js/audit_log/index.js
+++ templates/static/js/audit_log/index.js
@@ -1,36 +1,38 @@
-$.ajax({
-    url: '/serback/get_all_audit_logs/', 
-    method: 'GET', 
-    dataType: 'JSON', 
-    success: function(res) {
-        let txt = '<table class="table table-striped">';
-        txt += '<thead>';
-        txt += '<tr>';
-        txt += '<th>id</th><th>user_id</th><th>action</th><th>parameter_in</th><th>parameter_out</th><th>created_at</th><th>updated_at</th>';
-        txt += '</tr>';
-        txt += '</thead>';
-        txt += '<tbody>';
-        if (res.status == 0) {
-            for (let i in res.data) {
+$(function() {
+    $.ajax({
+        url: '/serback/get_all_audit_logs/', 
+        method: 'GET', 
+        dataType: 'JSON', 
+        success: function(res) {
+            var txt = '<table class="table table-striped">';
+            txt += '<thead>';
+            txt += '<tr>';
+            txt += '<th>id</th><th>user_id</th><th>action</th><th>parameter_in</th><th>parameter_out</th><th>created_at</th><th>updated_at</th>';
+            txt += '</tr>';
+            txt += '</thead>';
+            txt += '<tbody>';
+            if (res.status == 0) {
+                for (var i in res.data) {
+                    txt += '<tr>';
+                    txt += '<td>' + res.data[i].id + '</td>';
+                    txt += '<td>' + res.data[i].user_id + '</td>';
+                    txt += '<td>' + res.data[i].action + '</td>';
+                    txt += '<td>' + res.data[i].parameter_in + '</td>';
+                    txt += '<td>' + res.data[i].parameter_out + '</td>';
+                    txt += '<td>' + res.data[i].created_at + '</td>';
+                    txt += '<td>' + res.data[i].updated_at + '</td>';
+                    txt += '</tr>';
+                }
+            }
+            
+            if (res.status == -1) {
                 txt += '<tr>';
-                txt += '<td>' + res.data[i].id + '</td>';
-                txt += '<td>' + res.data[i].user_id + '</td>';
-                txt += '<td>' + res.data[i].action + '</td>';
-                txt += '<td>' + res.data[i].parameter_in + '</td>';
-                txt += '<td>' + res.data[i].parameter_out + '</td>';
-                txt += '<td>' + res.data[i].created_at + '</td>';
-                txt += '<td>' + res.data[i].updated_at + '</td>';
+                txt += '<td colspan="7">' + res.ret_val + '</td>';
                 txt += '</tr>';
             }
+            txt += '</tbody>';
+            txt += '</table>';
+            $('#main-content').html(txt);
         }
-        
-        if (res.status == -1) {
-            txt += '<tr>';
-            txt += '<td colspan="7">' + res.ret_val + '</td>';
-            txt += '</tr>';
-        }
-        txt += '</tbody>';
-        txt += '</table>';
-        $('#main-content').html(txt);
-    }
+    });
 });
