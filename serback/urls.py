16477da25af644a9f0dac19866fed9dd72ac3670
ericyou-times-transform <76938542+ericyou-times-transform@users.noreply.github.com>
2021-06-16 17:55:32 +0800

    update "get_all_shop_clicked_page" API's URL

--- serback/urls.py
+++ serback/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('get_all_audit_logs_page/', views.get_all_audit_logs_page),
-    path('get_all_audit_logs/', views.get_all_audit_logs)
+    path('get_all_audit_logs/', views.get_all_audit_logs), 
+    path('get_all_shop_clicked_page/', views.get_all_shop_clicked_page), 
+    path('get_all_shop_clicked/', views.get_all_shop_clicked)
 ]
