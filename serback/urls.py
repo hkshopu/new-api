514daad6d467676c4f052a46b5481a268ea61006
ericyou-times-transform <76938542+ericyou-times-transform@users.noreply.github.com>
2021-06-17 14:49:14 +0800

    add "get_all_product_clicked" API's URL

--- serback/urls.py
+++ serback/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('get_all_audit_logs_page/', views.get_all_audit_logs_page),
     path('get_all_audit_logs/', views.get_all_audit_logs), 
     path('get_all_shop_clicked_page/', views.get_all_shop_clicked_page), 
-    path('get_all_shop_clicked/', views.get_all_shop_clicked)
+    path('get_all_shop_clicked/', views.get_all_shop_clicked), 
+    path('get_all_product_clicked_page/', views.get_all_product_clicked_page), 
+    path('get_all_product_clicked/', views.get_all_product_clicked)
 ]
