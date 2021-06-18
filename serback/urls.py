903947a5bd388c5ae42b2cdbfb391e1cbcda3d15
ericyou-times-transform <76938542+ericyou-times-transform@users.noreply.github.com>
2021-06-18 15:31:37 +0800

    add "get_all_shop_browsed" API's URL

--- serback/urls.py
+++ serback/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('get_all_shop_clicked_page/', views.get_all_shop_clicked_page), 
     path('get_all_shop_clicked/', views.get_all_shop_clicked), 
     path('get_all_product_clicked_page/', views.get_all_product_clicked_page), 
-    path('get_all_product_clicked/', views.get_all_product_clicked)
+    path('get_all_product_clicked/', views.get_all_product_clicked), 
+    path('get_all_shop_browsed_page/', views.get_all_shop_browsed_page), 
+    path('get_all_shop_browsed', views.get_all_shop_browsed)
 ]
