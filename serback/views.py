4a0bd7b7ef41f0bcb3398403cb876f0d06c973f8
ericyou-times-transform <76938542+ericyou-times-transform@users.noreply.github.com>
2021-06-18 16:52:44 +0800

    add "get_all_product_browsed" API

--- serback/views.py
+++ serback/views.py
@@ -136,3 +136,33 @@ def get_all_shop_browsed(request):
                 })
             response_data['ret_val'] = '取得 Shop_Browsed 資料成功!'
     return JsonResponse(response_data)
+# Product_Browsed 首頁
+def get_all_product_browsed_page(request):
+    template = get_template('product_browsed/index.html')
+    html = template.render()
+    return HttpResponse(html)
+# 取得 Product_Browsed 資料
+def get_all_product_browsed(request):
+    response_data = {
+        'status': 0, 
+        'ret_val': '', 
+        'data': []
+    }
+    if request.method == 'GET':
+        if response_data['status'] == 0:
+            datas_of_product_browsed = models.Product_Browsed.objects.all()
+            if len(datas_of_product_browsed) == 0:
+                response_data['status'] = -1
+                response_data['ret_val'] = '暫無資料!'
+
+        if response_data['status'] == 0:
+            for data in datas_of_product_browsed:
+                response_data['data'].append({
+                    'id': data.id, 
+                    'product_id': data.product_id, 
+                    'user_id': data.user_id, 
+                    'created_at': data.created_at, 
+                    'updated_at': data.updated_at
+                })
+            response_data['ret_val'] = '取得 Product_Browsed 資料成功!'
+    return JsonResponse(response_data)
