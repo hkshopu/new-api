78ade12a32b13d2c17d8c0b7cf368c11296c8f3b
ericyou-times-transform <76938542+ericyou-times-transform@users.noreply.github.com>
2021-06-21 16:25:58 +0800

    update "send_invitation_testing_mail" HTML page

--- templates/static/js/invitation_testing_mail/index.js
+++ templates/static/js/invitation_testing_mail/index.js
@@ -1,16 +1,17 @@
 $(function() {
-    $.confirm({
-        title: '確定寄出邀請信', 
-        content: '確定要寄出邀請信嗎?', 
-        buttons: {
-            confirm: function() {
-                $('#send-invitation-testing-mail-form').on('submit', function(e) {
-                    e.preventDefault();
+    $('#send-invitation-testing-mail-form').on('submit', function(e) {
+        e.preventDefault();
+        $.confirm({
+            title: '確定寄出邀請信', 
+            content: '確定要寄出邀請信嗎?', 
+            buttons: {
+                confirm: function() {
                     $.post('/user/send_invitation_testing_mail/', {email: $('textarea[name="email"]').val()}, function(res) {
                         alert(res.ret_val);
                     }, 'JSON');
-                });
+                }, 
+                cancel: function() {}
             }
-        }
+        });
     });
 });
