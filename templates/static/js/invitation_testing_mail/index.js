57143338df08e7231be5dbd8a5ac5af307ec5e07
ericyou-times-transform <76938542+ericyou-times-transform@users.noreply.github.com>
2021-06-21 16:07:25 +0800

    update jQuery confirmation function

--- templates/static/js/invitation_testing_mail/index.js
+++ templates/static/js/invitation_testing_mail/index.js
@@ -1,11 +1,19 @@
 $(function() {
-    var confirm = confirm('確定要寄出邀請信嗎?');
-    if (confirm) {
-        $('#send-invitation-testing-mail-form').on('submit', function(e) {
-            e.preventDefault();
-            $.post('/user/send_invitation_testing_mail/', {email: $('textarea[name="email"]').val()}, function(res) {
-                alert(res.ret_val);
-            }, 'JSON');
-        });
-    }
+    $.confirm({
+        title: '確定寄出邀請信', 
+        content: '確定要寄出邀請信嗎?', 
+        buttons: {
+            '取消': function() {}, 
+            '確定': {
+                action: function() {
+                    $('#send-invitation-testing-mail-form').on('submit', function(e) {
+                        e.preventDefault();
+                        $.post('/user/send_invitation_testing_mail/', {email: $('textarea[name="email"]').val()}, function(res) {
+                            alert(res.ret_val);
+                        }, 'JSON');
+                    });
+                }
+            }
+        }
+    });
 });
