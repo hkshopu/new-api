84a031e075dc0a0fddf08fd885386733dd8ea779
chrischen <chrischen@times-transform.com>
2021-07-13 18:37:25 +0800

    update_detail

--- user_detail/views.py
+++ user_detail/views.py
@@ -79,7 +79,7 @@ def update_detail(request):
         if responseData['status']==0:
             for user in users:
                 if user_name !='':
-                    if not(re.match('^[A-Za-z]{3,45}$', accountName)):
+                    if not(re.match('^[A-Za-z]{3,45}$', user_name)):
                         responseData['status'] = -6
                         responseData['ret_val'] = '用戶名稱格式錯誤!'
                         return JsonResponse(responseData)
