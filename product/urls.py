c26367a30dd870f7d6d27d036b85a1e490f52160
chrischen <chrischen@times-transform.com>
2021-07-15 15:14:41 +0800

    product_list url

--- product/urls.py
+++ product/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('<id>/product_info/', views.product_info),
     path('<id>/product_info_forAndroid/', views.product_info_forAndroid),
     path('<id>/update/', views.update),  
-    path('<id>/<keyword>/<product_status>/<quantity>/product_list/', views.product_list),
+    path('product_list/', views.product_list),
     path('update_product_status/', views.update_product_status),
     path('update_product_status_forAndroid/', views.update_product_status_forAndroid),
     path('<id>/delete_product/', views.delete_product),
